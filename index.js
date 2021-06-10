1ca48d910150e1233976a4765d8b3b773665e391
Daniel Poss <dpatposs@msn.com>
2021-06-10 08:18:50 -0500

    Adding nodes_modules and updating tag to v1.4

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const discordPayload = require('./discord-build-success-webhook');
 
 try {
-    const discordWebhookUrl = core.getInput('discord-hook-url:');
-    console.log(`Url ${discordWebhookUrl}!`);
+    const discordWebhookUrl = core.getInput('discord-hook-url');
+    const title = core.getInput('title');
+    const message = core.getInput('message');
+    const messageColor = core.getInput('message-color');
+    console.log(`Url > ${discordWebhookUrl}`);
+    console.log(`Title > ${title}`);
+    console.log(`Message > ${message}`);
+    console.log(`Message Color > ${messageColor}`);
+    let discordPayloadCopy = JSON.parse(JSON.stringify(discordPayload));
+    discordPayloadCopy.username = "Test";
+    discordPayloadCopy.embeds[0].title = title;
+    discordPayloadCopy.embeds[0].description = message;
+    discordPayloadCopy.embeds[0].color = messageColor;
+    console.log(`Discord Payload: ${JSON.stringify(discordPayloadCopy)}`);
     const successful = true;
     core.setOutput("successful", successful);
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
