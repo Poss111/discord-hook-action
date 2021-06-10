f88e7ab21554683e6245cd947d5f5c4eaa59cb54
Daniel Poss <dpatposs@msn.com>
2021-06-10 07:46:21 -0500

    Updated dockerfile for public repo

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
-# Container image that runs your code
 FROM node:14.16.1-alpine
-WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 COPY . .
--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    // `who-to-greet` input defined in action metadata file
     const discordWebhookUrl = core.getInput('discord-hook-url:');
     console.log(`Url ${discordWebhookUrl}!`);
     const successful = true;
     core.setOutput("successful", successful);
-    // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
 } catch (error) {
